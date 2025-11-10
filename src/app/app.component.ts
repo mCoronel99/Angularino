9b4976d8a69f50d9042f0be5309d6337ba011cb5
Michele Coronel Moron <gidan193@gmail.com>
2025-11-10 09:34:48 +0100

    push alla detective conan che insegue Lupin III

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users/dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular19';
+ users = DUMMY_USERS;
+ selectedUserId = 'u1';
+
+ get selectedUser() {
+  return this.users.find(user => user.id === this.selectedUserId);
+ }
+ onSelectUser(id: string) {
+  this.selectedUserId= id;
+
+}
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, EventEmitter, Input, output, Output, signal } from '@angular/core';
+import {DUMMY_USERS} from '../dummy-users/dummy-users';
+import { type User } from './user.model';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+@Input ({required: true}) user!:User;
+@Input ({required: true}) selected!: boolean;
+@Output() select = new EventEmitter<string>();
+
+get imagePath(){
+  return 'users/' + this.user.avatar;
+}
+
+onSelectUser(){
+this.select.emit(this.user.id);
+}
+}
